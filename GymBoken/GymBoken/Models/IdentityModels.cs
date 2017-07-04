306cdbf0a15fadaaed7fae5e98fab755d8a8f39b
Peter Nilsson <incido@gmail.com>
2017-07-04 10:16:20 +0200

    Version 0.1 Done

--- GymBoken/GymBoken/Models/IdentityModels.cs
+++ GymBoken/GymBoken/Models/IdentityModels.cs
@@ -3,13 +3,15 @@ using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
+using System.Collections.Generic;
 
 namespace GymBoken.Models
 {
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
-        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+		public virtual ICollection<GymClass> AttendedClasses { get; set; }
+		public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
             var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
@@ -20,7 +22,9 @@ namespace GymBoken.Models
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
-        public ApplicationDbContext()
+		public DbSet<GymClass> GymClasses { get; set; } //= new DBSet<GymClass>();
+
+		public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
         }
