853d54cf4ca3850bf7f4eb43318220dd2c99365c
TheMightyAi <andreaseriksvensson97@gmail.com>
2017-07-04 13:57:41 +0200

    Version0.4

--- GymBoken/GymBoken/Migrations/Configuration.cs
+++ GymBoken/GymBoken/Migrations/Configuration.cs
@@ -1,31 +1,78 @@
 namespace GymBoken.Migrations
 {
-    using System;
-    using System.Data.Entity;
-    using System.Data.Entity.Migrations;
-    using System.Linq;
+	using GymBoken.Models;
+	using Microsoft.AspNet.Identity;
+	using Microsoft.AspNet.Identity.EntityFramework;
+	using System;
+	using System.Data.Entity;
+	using System.Data.Entity.Migrations;
+	using System.Linq;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<GymBoken.Models.ApplicationDbContext>
-    {
-        public Configuration()
-        {
-            AutomaticMigrationsEnabled = false;
-        }
+	internal sealed class Configuration : DbMigrationsConfiguration<GymBoken.Models.ApplicationDbContext>
+	{
+		public Configuration()
+		{
+			AutomaticMigrationsEnabled = false;
+		}
 
-        protected override void Seed(GymBoken.Models.ApplicationDbContext context)
-        {
-            //  This method will be called after migrating to the latest version.
+		protected override void Seed(GymBoken.Models.ApplicationDbContext context)
+		{
+			//  This method will be called after migrating to the latest version.
 
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
-        }
-    }
+			//  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+			//  to avoid creating duplicate seed data. E.g.
+			//
+			//    context.People.AddOrUpdate(
+			//      p => p.FullName,
+			//      new Person { FullName = "Andrew Peters" },
+			//      new Person { FullName = "Brice Lambson" },
+			//      new Person { FullName = "Rowan Miller" }
+			//    );
+			//
+			RoleStore<IdentityRole> roleStore = new RoleStore<IdentityRole>(context);
+			RoleManager<IdentityRole> roleManager = new RoleManager<IdentityRole>(roleStore);
+
+			string[] roleNames = new[] { "Admin", "Member" };
+			foreach (string roleName in roleNames)
+			{
+				if (!context.Roles.Any(r => r.Name == roleName))
+				{
+					IdentityRole role = new IdentityRole { Name = roleName };
+					IdentityResult result = roleManager.Create(role);
+					if (!result.Succeeded)
+					{
+						throw new Exception(string.Join("\n", result.Errors));
+					}
+				}
+			}
+			UserStore<ApplicationUser> userStore = new UserStore<ApplicationUser>(context);
+			UserManager<ApplicationUser> userManager = new UserManager<ApplicationUser>(userStore);
+			string[] emails = new[] { "ai@gym.se","admin@gymbokning.se", "admin@admin.ad", "editor@gym.se", "bob@gym.se" };
+			foreach(string email in emails)
+			{
+				if(!context.Users.Any(u=>u.UserName == email))
+				{
+					ApplicationUser user = new ApplicationUser { UserName = email, Email = email };
+					var result = userManager.Create(user, "foobar");
+
+					if (!result.Succeeded)
+					{
+						throw new Exception(string.Join("\n", result.Errors));
+					}
+				}
+		
+			}
+			ApplicationUser adminUser = userManager.FindByName("admin@admin.ad");
+			userManager.AddToRole(adminUser.Id, "Admin");
+
+			adminUser = userManager.FindByName("admin@gymbokning.se");
+			userManager.AddToRole(adminUser.Id, "Admin");
+
+			foreach (ApplicationUser user in context.Users.ToList().Where(u => u.Email != "admin@admin.ad" && u.Email != "admin@gymbokning.se"))
+			{
+				userManager.AddToRole(user.Id, "Member");
+			}
+			
+		}
+	}
 }
