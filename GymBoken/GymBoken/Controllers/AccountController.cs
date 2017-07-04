4fc5c6a1b6729ee6bc7ae3bab625a8dfb078556a
Mattias Rehnvall <Rehnvall15@gmail.com>
2017-07-04 13:57:52 +0200

    Version 0.4 Done

--- GymBoken/GymBoken/Controllers/AccountController.cs
+++ GymBoken/GymBoken/Controllers/AccountController.cs
@@ -163,7 +163,7 @@ namespace GymBoken.Controllers
                     // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
                     // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
 
-                    return RedirectToAction("Index", "GymGlasses");
+                    return RedirectToAction("Index", "GymClasses");
                 }
                 AddErrors(result);
             }
@@ -392,7 +392,7 @@ namespace GymBoken.Controllers
         public ActionResult LogOff()
         {
             AuthenticationManager.SignOut(DefaultAuthenticationTypes.ApplicationCookie);
-            return RedirectToAction("Index", "GymGlasses");
+            return RedirectToAction("Index", "GymClasses");
         }
 
         //
@@ -449,7 +449,7 @@ namespace GymBoken.Controllers
             {
                 return Redirect(returnUrl);
             }
-            return RedirectToAction("Index", "GymGlasses");
+            return RedirectToAction("Index", "GymClasses");
         }
 
         internal class ChallengeResult : HttpUnauthorizedResult
