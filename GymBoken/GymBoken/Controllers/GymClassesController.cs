dfd8e84a33a63601123d9acb88c19f08e0dbc05f
Peter Nilsson <incido@gmail.com>
2017-07-04 10:48:59 +0200

    Version 0.2 Done

--- GymBoken/GymBoken/Controllers/GymClassesController.cs
+++ GymBoken/GymBoken/Controllers/GymClassesController.cs
@@ -10,118 +10,140 @@ using GymBoken.Models;
 
 namespace GymBoken.Controllers
 {
-    public class GymClassesController : Controller
-    {
-        private ApplicationDbContext db = new ApplicationDbContext();
+	public class GymClassesController : Controller
+	{
+		private ApplicationDbContext db = new ApplicationDbContext();
 
-        // GET: GymClasses
-        public ActionResult Index()
-        {
-            return View(db.GymClasses.ToList());
-        }
+		// GET: GymClasses
+		public ActionResult Index()
+		{
+			return View(db.GymClasses.ToList());
+		}
 
-        // GET: GymClasses/Details/5
-        public ActionResult Details(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            GymClass gymClass = db.GymClasses.Find(id);
-            if (gymClass == null)
-            {
-                return HttpNotFound();
-            }
-            return View(gymClass);
-        }
+		// GET: GymClasses/Details/5
+		public ActionResult Details(int? id)
+		{
+			if (id == null)
+			{
+				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+			}
+			GymClass gymClass = db.GymClasses.Find(id);
+			if (gymClass == null)
+			{
+				return HttpNotFound();
+			}
+			return View(gymClass);
+		}
 
-        // GET: GymClasses/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
+		// GET: GymClasses/Create
+		public ActionResult Create()
+		{
+			return View();
+		}
 
-        // POST: GymClasses/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "Id,Name,StartTime,Duration,Description")] GymClass gymClass)
-        {
-            if (ModelState.IsValid)
-            {
-                db.GymClasses.Add(gymClass);
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
+		// POST: GymClasses/Create
+		// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+		// more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult Create([Bind(Include = "Id,Name,StartTime,Duration,Description")] GymClass gymClass)
+		{
+			if (ModelState.IsValid)
+			{
+				db.GymClasses.Add(gymClass);
+				db.SaveChanges();
+				return RedirectToAction("Index");
+			}
 
-            return View(gymClass);
-        }
+			return View(gymClass);
+		}
 
-        // GET: GymClasses/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            GymClass gymClass = db.GymClasses.Find(id);
-            if (gymClass == null)
-            {
-                return HttpNotFound();
-            }
-            return View(gymClass);
-        }
+		public ActionResult BookingToggle(int? id)
+		{
+			if (id == null)
+			{
+				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+			}
+			GymClass CurrentClass = db.GymClasses.Where(m => m.Id == id).FirstOrDefault();
+			ApplicationUser CurrentUser = db.Users.Where(u => u.UserName == User.Identity.Name).FirstOrDefault();
 
-        // POST: GymClasses/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Id,Name,StartTime,Duration,Description")] GymClass gymClass)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(gymClass).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            return View(gymClass);
-        }
+			if (CurrentClass.AttendingMembers.Contains(CurrentUser))
+			{
+				CurrentClass.AttendingMembers.Remove(CurrentUser);
+				db.SaveChanges();
+			}
+			else
+			{
+				CurrentClass.AttendingMembers.Add(CurrentUser);
+				db.SaveChanges();
+			}
+			return RedirectToAction("Index");
+		}
 
-        // GET: GymClasses/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            GymClass gymClass = db.GymClasses.Find(id);
-            if (gymClass == null)
-            {
-                return HttpNotFound();
-            }
-            return View(gymClass);
-        }
+		// GET: GymClasses/Edit/5
+		public ActionResult Edit(int? id)
+		{
+			if (id == null)
+			{
+				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+			}
+			GymClass gymClass = db.GymClasses.Find(id);
+			if (gymClass == null)
+			{
+				return HttpNotFound();
+			}
+			return View(gymClass);
+		}
 
-        // POST: GymClasses/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            GymClass gymClass = db.GymClasses.Find(id);
-            db.GymClasses.Remove(gymClass);
-            db.SaveChanges();
-            return RedirectToAction("Index");
-        }
+		// POST: GymClasses/Edit/5
+		// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+		// more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult Edit([Bind(Include = "Id,Name,StartTime,Duration,Description")] GymClass gymClass)
+		{
+			if (ModelState.IsValid)
+			{
+				db.Entry(gymClass).State = EntityState.Modified;
+				db.SaveChanges();
+				return RedirectToAction("Index");
+			}
+			return View(gymClass);
+		}
 
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                db.Dispose();
-            }
-            base.Dispose(disposing);
-        }
-    }
+		// GET: GymClasses/Delete/5
+		public ActionResult Delete(int? id)
+		{
+			if (id == null)
+			{
+				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+			}
+			GymClass gymClass = db.GymClasses.Find(id);
+			if (gymClass == null)
+			{
+				return HttpNotFound();
+			}
+			return View(gymClass);
+		}
+
+		// POST: GymClasses/Delete/5
+		[HttpPost, ActionName("Delete")]
+		[ValidateAntiForgeryToken]
+		public ActionResult DeleteConfirmed(int id)
+		{
+			GymClass gymClass = db.GymClasses.Find(id);
+			db.GymClasses.Remove(gymClass);
+			db.SaveChanges();
+			return RedirectToAction("Index");
+		}
+
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing)
+			{
+				db.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+	}
 }
