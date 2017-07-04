0d0d10c3da009f1be78d4a8a189970192aeaec48
GIGAR KHALIL <gigarkhalil@hotmail.com>
2017-07-04 10:18:50 +0200

    Version 0.1

---
+++ GymBoken/GymBoken/Models/GymClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace GymBoken.Models
+{
+    public class GymClass
+    {
+        
+        public int Id { get; set; }
+        string Name { get; set; }
+        DateTime StartTime { get; set; }
+        TimeSpan Duration { get; set; }
+        DateTime EndTime { get { return StartTime + Duration; } }
+        public  string Description { get; set; }
+        public virtual ICollection<ApplicationUser> AttendingMembers { get; set; }
+    }
+}
