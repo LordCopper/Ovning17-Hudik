6f2d033ca664d178d928eaebd958d1cba3f80724
Peter Nilsson <incido@gmail.com>
2017-07-04 13:57:38 +0200

    Version 0.4 Done

--- GymBoken/GymBoken/Migrations/Configuration.cs
+++ GymBoken/GymBoken/Migrations/Configuration.cs
@@ -1,6 +1,9 @@
 namespace GymBoken.Migrations
 {
-    using System;
+	using GymBoken.Models;
+	using Microsoft.AspNet.Identity;
+	using Microsoft.AspNet.Identity.EntityFramework;
+	using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
@@ -14,18 +17,63 @@ namespace GymBoken.Migrations
 
         protected override void Seed(GymBoken.Models.ApplicationDbContext context)
         {
-            //  This method will be called after migrating to the latest version.
+			//  This method will be called after migrating to the latest version.
 
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
-        }
-    }
+			//  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+			//  to avoid creating duplicate seed data. E.g.
+			//
+			//    context.People.AddOrUpdate(
+			//      p => p.FullName,
+			//      new Person { FullName = "Andrew Peters" },
+			//      new Person { FullName = "Brice Lambson" },
+			//      new Person { FullName = "Rowan Miller" }
+			//    );
+			//
+
+			var roleStore = new RoleStore<IdentityRole>(context);
+			var roleManager = new RoleManager<IdentityRole>(roleStore);
+
+			string[] roleNames = new[] { "Admin", "Member" };
+			foreach (string roleName in roleNames)
+			{
+				if (!context.Roles.Any(r => r.Name == roleName))
+				{
+					var role = new IdentityRole { Name = roleName };
+					var result = roleManager.Create(role);
+					if (!result.Succeeded)
+					{
+						throw new Exception(string.Join("\n", result.Errors));
+					}
+				}
+			}
+
+			var userStore = new UserStore<ApplicationUser>(context);
+			var userManager = new UserManager<ApplicationUser>(userStore);
+
+			string[] emails = new[] { "god@heaven.org", "nisse@hult.se", "a@b.com", "epa@bepa.net", "admin@gymbokning.se" };
+			foreach (string email in emails)
+			{
+				if (!context.Users.Any(u => u.Email == email))
+				{
+					var user = new ApplicationUser { UserName = email, Email = email };
+					var result = userManager.Create(user, "foobar");
+					if (!result.Succeeded)
+					{
+						throw new Exception(string.Join("\n", result.Errors));
+					}
+				}
+			}
+
+			var adminUser = userManager.FindByEmail("god@heaven.org");
+			userManager.AddToRole(adminUser.Id, "Admin");
+
+			adminUser = userManager.FindByEmail("admin@gymbokning.se");
+			userManager.AddToRole(adminUser.Id, "Admin");
+
+			foreach (var user in userManager.Users.ToList().Where(u => (u.Email != "god@heaven.org" && u.Email != "admin@gymbokning.se")))
+			{
+				userManager.AddToRole(user.Id, "Member");
+			}
+		}
+	}
 }
