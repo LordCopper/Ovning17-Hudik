f0d5698962de1fd64da13792e02944bdcaba4e39
Basse <s.cedendahl@gmail.com>
2017-07-04 16:12:56 +0200

    Version 0.9 Done

--- GymBoken/GymBoken/Migrations/Configuration.cs
+++ GymBoken/GymBoken/Migrations/Configuration.cs
@@ -4,6 +4,7 @@ namespace GymBoken.Migrations
     using Microsoft.AspNet.Identity;
     using Microsoft.AspNet.Identity.EntityFramework;
     using System;
+    using System.Collections.Generic;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
@@ -78,7 +79,33 @@ namespace GymBoken.Migrations
             {
                 userManager.AddToRole(user.Id, "Member");
             }
+            context.SaveChanges();
 
+            GymClass[] gym = new GymClass[] {
+             new GymClass
+            {
+                Name = "Kicking",
+                Description = "Sparka loss",
+                Duration = new TimeSpan(0, 30, 0),
+                StartTime = new DateTime(1999, 02, 06),
+               AttendingMembers = new List<ApplicationUser>()
+            },
+             new GymClass
+            {
+                Name = "Running",
+                Description = "Hej hej",
+                Duration = new TimeSpan(0, 30, 0),
+                StartTime = new DateTime(2019, 02, 06),
+                AttendingMembers = new List<ApplicationUser>()
+            }
+
+            };
+            gym[0].AttendingMembers.Add(adminUser);
+            gym[1].AttendingMembers.Add(adminUser);
+            foreach(GymClass g in gym)
+            {
+                context.GymClasses.Add(g);
+            }
         }
     }
 }
